1a7f948db6ceb29a738a8339dea3c5292cd3bd7b
HuttonR <hutton.rounds@thebronson.com>
2015-12-08 17:08:42 -0800

    Version bump.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'huttonr:bootstrap3-assets',
   summary: 'By itself this package does nothing.  It contains assets for use by huttonr:bootstrap3.',
-  version: '3.3.5_5',
+  version: '3.3.5_6',
   git: 'https://github.com/huttonr/bootstrap3-assets',
   documentation: 'README.md'
 });
